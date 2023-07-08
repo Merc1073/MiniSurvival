d097af032c2b2bbfdee1d23e5ddf643ef7f7c988
Merc1073 <sergeikoblov2000@gmail.com>
2023-07-08 21:22:11 +0100

    Project files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+
+    public Vector3 movement;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector3(horizontalMovement, 0, verticalMovement);
+
+
+        rb.AddForce(movement * speed);
+    }
+}
